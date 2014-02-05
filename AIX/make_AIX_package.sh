50431cc29cac3c2334a9193dc914a32d4845d17c
Andreas Leibl <andreas@leibl.co.uk>
2014-02-05 14:57:21 -0600

    Unifying the AIX package names and making them more robust

--- AIX/make_AIX_package.sh
+++ AIX/make_AIX_package.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/ksh
 
 echo "Creating package..."
+# clean up directories of previous builds
+rm -rf .info
+rm -rf tmp
+# create the file tree
 mkdir -p opt/CertNanny/bin
 mkdir -p opt/CertNanny/inst
 mkdir -p usr/bin
@@ -9,8 +13,10 @@ cp sscep_static opt/CertNanny/bin
 cp sscep_dyn opt/CertNanny/bin
 cp AIX/sscep.pre-install.sh opt/CertNanny/inst
 cp AIX/sscep.pre-deinstall.sh opt/CertNanny/inst
+# compute the version number (VRML)
 version=$(head -n 1 VERSION)
 version="$version.0"
+# create the template and building the package
 sed "s/VERSIONINFO/$version/" < AIX/lpp_template.in | sed "s#__PACKAGINGDIR__#$PWD#" > AIX/lpp_template
 mkinstallp -d . -T AIX/lpp_template
 
